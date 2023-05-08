0ec8a0ec283063d225c2fdf87aa36f12dea50c61
sohrabezzati <s.ezzati.30.dec.1999@gmail.com>
2023-05-08 13:29:23 +0430

    all wedgit in main created

--- lib/main.dart
+++ lib/main.dart
@@ -34,6 +34,47 @@ class _StoryPageState extends State<StoryPage> {
             fit: BoxFit.cover,
           ),
         ),
+        padding: const EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
+        constraints: const BoxConstraints.expand(),
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Expanded(
+                flex: 12,
+                child: Center(
+                  child: Text(
+                    '',
+                    style: TextStyle(fontSize: 25.0),
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    '',
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              Expanded(
+                flex: 2,
+                child: Visibility(
+                  visible: true,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text(''),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
